06bb11676e8c807e8d77b86289b8714a8d3a60aa
VictorCruzGo <cruz.gomez.victor@gmail.com>
2021-02-05 11:41:52 -0400

    fs:Imagen borrar cliente anterior actualizar-96

--- workspace/spring-boot-backend-apirest/src/main/java/com/ar/springboot/backend/apirest/controllers/ClienteRestController.java
+++ workspace/spring-boot-backend-apirest/src/main/java/com/ar/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,5 +1,6 @@
 package com.ar.springboot.backend.apirest.controllers;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -220,6 +221,16 @@ public class ClienteRestController {
 		Map<String,Object> response=new HashMap<>();
 		
 		try {
+			Cliente cliente=clienteService.findById(id);
+			String nombreFotoAnterior=cliente.getFoto();
+			if (nombreFotoAnterior!=null && nombreFotoAnterior.length()>0) {
+				Path rutaFotoAnterior=Paths.get("uploads").resolve(nombreFotoAnterior).toAbsolutePath();
+				File archivoFotoAnterior=rutaFotoAnterior.toFile();
+				if (archivoFotoAnterior.exists() && archivoFotoAnterior.canRead()) {
+					archivoFotoAnterior.delete();
+				}				
+			}
+			
 			clienteService.delete(id);	
 		} catch (DataAccessException e) {
 			response.put("mensaje", "Error al eliminar el cliente de la base de datos [backend]");
@@ -249,7 +260,17 @@ public class ClienteRestController {
 				response.put("mensaje", "Error al subir la imagen del cliente"+nombreArchivo);
 				response.put("error", e.getMessage().concat(": ").concat(e.getCause().getMessage()));
 				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-			}					
+			}		
+			
+			String nombreFotoAnterior=cliente.getFoto();
+			if (nombreFotoAnterior!=null && nombreFotoAnterior.length()>0) {
+				Path rutaFotoAnterior=Paths.get("uploads").resolve(nombreFotoAnterior).toAbsolutePath();
+				File archivoFotoAnterior=rutaFotoAnterior.toFile();
+				if (archivoFotoAnterior.exists() && archivoFotoAnterior.canRead()) {
+					archivoFotoAnterior.delete();
+				}				
+			}
+			
 			cliente.setFoto(nombreArchivo);
 			
 			clienteService.save(cliente);
